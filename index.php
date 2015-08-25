d1242f176f6979cb816da1b83b7b66142155ae8d
unknown <kilice@LMB-113B-04.s2.rpn.ch>
2015-08-25 08:14:48 +0200

    hh

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+        <title>4MPI4I2</title>
+        <meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/default.css">
+		<link rel="stylesheet" type="text/css" href="css/slider.css">
+
+
+</head>
+
+<body>
+
+	<div id="wrapper_header">
+		<div id="header" class="inner">
+			<ul class="nav" >
+				<li><a href="#">Accueil</a></li>
+				<li><a href="evenement.php">Evenement</a></li>
+				<li><a href="#">Mini-jeux</a></li>
+			</ul>
+		</div>
+	</div>
+	
+
+	<!-- Start cssSlider.com -->
+	<div class='csslider1 autoplay '>
+		<input name="cs_anchor1" id='cs_slide1_0' type="radio" class='cs_anchor slide' >
+		<input name="cs_anchor1" id='cs_slide1_1' type="radio" class='cs_anchor slide' >
+		<input name="cs_anchor1" id='cs_slide1_2' type="radio" class='cs_anchor slide' >
+		<input name="cs_anchor1" id='cs_play1' type="radio" class='cs_anchor' checked>
+		<input name="cs_anchor1" id='cs_pause1' type="radio" class='cs_anchor' >
+		<ul>
+			<div style="width: 100%; visibility: hidden; font-size: 0px; line-height: 0;">
+				<img src="https://upload.wikimedia.org/wikipedia/commons/f/f6/Bangkok_skytrain_sunset.jpg" style="width: 100%;">
+			</div>
+			<li class='num0 img'>
+				 <a href="#" target=""><img src='https://upload.wikimedia.org/wikipedia/commons/4/49/Bangkok_Skyline_7.jpg' alt='Buns' title='Buns' /> </a> 
+			</li>
+			<li class='num1 img'>
+				 <a href="#" target=""><img src='https://upload.wikimedia.org/wikipedia/commons/4/49/Bangkok_Skyline_7.jpg' alt='Croissant' title='Croissant' /> </a> 
+			</li>
+			<li class='num2 img'>
+				 <a href="#" target=""><img src='https://upload.wikimedia.org/wikipedia/commons/4/49/Bangkok_Skyline_7.jpg' alt='Lemon pie' title='Lemon pie' /> </a> 
+			</li>
+		
+		</ul>
+		<div class='cs_description'>
+			<label class='num0'>
+				<span class="cs_title"><span class="cs_wrapper">Buns</span></span>
+			</label>
+			<label class='num1'>
+				<span class="cs_title"><span class="cs_wrapper">Croissant</span></span>
+			</label>
+			<label class='num2'>
+				<span class="cs_title"><span class="cs_wrapper">Lemon pie</span></span>
+			</label>
+		</div>
+		
+		<div class='cs_arrowprev'>
+			<label class='num0' for='cs_slide1_0'></label>
+			<label class='num1' for='cs_slide1_1'></label>
+			<label class='num2' for='cs_slide1_2'></label>
+		</div>
+		<div class='cs_arrownext'>
+			<label class='num0' for='cs_slide1_0'></label>
+			<label class='num1' for='cs_slide1_1'></label>
+			<label class='num2' for='cs_slide1_2'></label>
+		</div>
+		
+		<div class='cs_bullets'>
+			<label class='num0' for='cs_slide1_0'>
+				<span class='cs_point'></span>
+				<span class='cs_thumb'><img src='' alt='Buns' title='' /></span>
+			</label>
+			<label class='num1' for='cs_slide1_1'>
+				<span class='cs_point'></span>
+				<span class='cs_thumb'><img src='' alt='Croissant' title='' /></span>
+			</label>
+			<label class='num2' for='cs_slide1_2'>
+				<span class='cs_point'></span>
+				<span class='cs_thumb'><img src='' alt='Lemon pie' title='' /></span>
+			</label>
+		</div>
+		</div>
+	<!-- End cssSlider.com -->
+   
+   
+   
+   <div id="wrapper_contenu">
+   
+		<div id="contenu" class="inner">
+		
+	
+		<!--
+		<form action="" method="post" class="form-login">
+
+        <div class="form__field">
+          <label ><span class="hidden">Username</span></label>
+          <input type="text" placeholder="Username" required>
+        </div>
+
+        <div class="form__field">
+          <label><span class="hidden">Password</span></label>
+          <input type="password"placeholder="Password" required>
+        </div>
+
+        <div class="form__field">
+          <input type="submit" value="Sign In">
+        </div>
+
+      </form>
+		-->
+
+	
+
+		
+		</div>
+   
+   </div>
+   
+   
+   <div id="wrapper_footer">
+		<div id="footer" class="inner">
+				
+			<div class="col_50">
+
+			</div>
+			
+			<div class="col_50">
+			
+			</div>
+			
+		</div>
+   </div>
+   
+</body>
+</html>
